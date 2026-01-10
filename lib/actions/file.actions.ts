5f3ddbd85813e6d2fe3969ca91d884a4f8ba1bda
dabestcode <pjanga@asu.edu>
2026-01-09 23:13:45 -0700

    minor card changes

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -2,10 +2,11 @@
 
 import { createAdminClient } from "../appwrite";
 import { InputFile } from "node-appwrite/file";
-import { ID } from "node-appwrite";
+import { ID, Models, Query } from "node-appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
+import { getCurrentUser } from "./user.actions";
 const handleError = (error: any, message: string) => {
   console.error(message, error);
   throw error;
@@ -50,5 +51,34 @@ export const uploadFile = async ({
     return parseStringify(newFile);
   } catch (error) {
     handleError(error, "Failed to upload file");
+  }
+};
+
+const createQueries = (currentUser: Models.Document) => {
+  const queries = [
+    Query.or([
+      Query.equal("owner", [currentUser.$id]),
+      Query.contains("users", [currentUser.$id]),
+    ]),
+  ];
+  return queries;
+};
+
+export const getFiles = async () => {
+  const { databases } = await createAdminClient();
+  try {
+    const currentUser = await getCurrentUser();
+    if (!currentUser) {
+      throw new Error("User not authenticated");
+    }
+    const queries = createQueries(currentUser);
+    const files = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      queries
+    );
+    return parseStringify(files);
+  } catch (error) {
+    handleError(error, "Failed to get files");
   }
 };
